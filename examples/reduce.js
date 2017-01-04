d33d0766c4e188b21326f532a80b2382c3db1e35
Clyde Smets <clyde@apnic.net>
2017-01-04 17:49:08 +1000

    Fixed typo

--- examples/reduce.js
+++ examples/reduce.js
@@ -2,4 +2,4 @@ const tests = require('./testObjects')
 
 // Reduce - add or subtract
 let totalAmount = tests.orders.reduce((sum, order) => sum + order.amount, 0)
-console.log("Total about: ", totalAmount);
+console.log("Total amout: ", totalAmount);
