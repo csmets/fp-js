3c75460685e94452da359fdb80d13b7ce1569fe8
Clyde Smets <clyde.smets@gmail.com>
2017-01-20 23:31:05 +1000

    Updated composition to compose infinite num of functions

--- examples/composition.js
+++ examples/composition.js
@@ -1,9 +1,20 @@
-const compose = (f, g) => x => f(g(x));
+const compose = (...funcs) => (...args) => {
+	const loop = (i, res) => {
+		if (i > 0) {
+			const count = i - 1;
+			return loop(count, [funcs[count].apply(this, res)]);
+		}
+		return res;
+	};
+	return loop(funcs.length, args)[0];
+};
 
 const toUpperCase = x => x.toUpperCase();
 
 const exclaim = x => `${x}!`;
 
-const shout = compose(toUpperCase, exclaim);
+const duplicate = x => `${x} ${x}`;
+
+const shout = compose(toUpperCase, exclaim, duplicate);
 
 console.log(shout('hello'));
