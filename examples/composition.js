88ef764ab150f32ba84bdf93c97bad40187d3af5
Clyde Smets <clyde.smets@gmail.com>
2017-02-05 22:28:05 +1000

    Fixed typo

--- examples/composition.js
+++ examples/composition.js
@@ -1,18 +1,18 @@
 /* 
- * Composition allows you to string a bunch of functions which well execute
+ * Composition allows you to string a bunch of functions which will execute
  * from left to right.
  */
 
 // Steal this function to use in your projects.
 const compose = (...funcs) => (...args) => {
-	const loop = (i, res) => {
-		if (i > 0) {
-			const count = i - 1;
-			return loop(count, [funcs[count].apply(this, res)]);
-		}
-		return res;
-	};
-	return loop(funcs.length, args)[0];
+    const loop = (i, res) => {
+        if (i > 0) {
+            const count = i - 1;
+            return loop(count, [funcs[count].apply(this, res)]);
+        }
+        return res;
+    };
+    return loop(funcs.length, args)[0];
 };
 
 const toUpperCase = x => x.toUpperCase();
